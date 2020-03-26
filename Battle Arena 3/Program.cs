2728c08fea32195189f3244bba13dbc9ed139931
31xmg2rtne <31xmg2rtne@goetheuniversitaet.onmicrosoft.com>
2020-03-26 01:16:47 +0100

    Updated to .net 4.7.2

--- Battle Arena 3/Program.cs
+++ Battle Arena 3/Program.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Battle_Arena_3
+﻿namespace Battle_Arena_3
 {
     class Program
     {
